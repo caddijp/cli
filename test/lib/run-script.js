a60c5a8e016051a44c9d5e2b5ffc6342eee30c07
Gar <gar+gh@danger.computer>
2021-03-10 16:11:34 -0800

    feat(help): refactor npm help/help-search
    Lots of dead code removed thanks to streamlining of logic.
    `npm help` `npm <command>` and `npm help-search` are all now separated
    concerns, handling their own use cases. `help` calls `help-search` as a
    last resort, but `npm <command>` no longer tries to wind its way through
    to `help-search` just to get the basic npm usage displayed.
    
    The `did you mean` output has been expanded. It now always suggests top
    level commands, scripts, and bins, and suggests them in the way they
    should be called.

--- test/lib/run-script.js
+++ test/lib/run-script.js
@@ -9,6 +9,11 @@ const npm = {
     json: false,
     parseable: false,
   },
+  commands: {
+    help: {
+      description: 'test help description',
+    },
+  },
   config: {
     settings: {
       'if-present': false,
@@ -261,20 +266,35 @@ t.test('try to run missing script', t => {
   npm.localPrefix = t.testdir({
     'package.json': JSON.stringify({
       scripts: { hello: 'world' },
+      bin: { goodnight: 'moon' },
     }),
   })
   t.test('no suggestions', t => {
     runScript.exec(['notevenclose'], er => {
       t.match(er, {
-        message: 'missing script: notevenclose',
+        message: 'Unknown command: "notevenclose"',
       })
       t.end()
     })
   })
-  t.test('suggestions', t => {
+  t.test('script suggestions', t => {
     runScript.exec(['helo'], er => {
       t.match(er, {
-        message: 'missing script: helo\n\nDid you mean this?\n    hello',
+        message: 'Unknown command: "helo"',
+      })
+      t.match(er, {
+        message: 'npm run hello',
+      })
+      t.end()
+    })
+  })
+  t.test('bin suggestions', t => {
+    runScript.exec(['goodneght'], er => {
+      t.match(er, {
+        message: 'Unknown command: "goodneght"',
+      })
+      t.match(er, {
+        message: 'npm exec goodnight',
       })
       t.end()
     })
